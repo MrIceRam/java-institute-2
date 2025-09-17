e8cd90fd09ab4f6af27f3218ce4aebdacfe9698e
MrIceRam <skyfarliv@gmail.com>
2025-09-17 15:39:01 +0300

    add 1.6

---
+++ Num1/Num6.java
@@ -0,0 +1,13 @@
+package Num1;
+
+public class Num6 {
+    public static void main(String[] args) {
+
+        for(double i=1;i<11;i++){
+
+        System.out.println("-----------------------------");
+        System.out.println(1/i);
+        }
+
+    }
+}
