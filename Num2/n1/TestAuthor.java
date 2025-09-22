42296b194de579e43010ae0529412842e7c65c00
MrIceRam <skyfarliv@gmail.com>
2025-09-22 22:02:11 +0300

    add N2.1

---
+++ Num2/n1/TestAuthor.java
@@ -0,0 +1,11 @@
+package Num2.n1;
+
+import java.util.Scanner;
+
+public class TestAuthor{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Author au = new Author(sc.next(),sc.next(),'m');
+        System.out.println(au);
+    }
+}
